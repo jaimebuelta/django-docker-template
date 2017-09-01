7129475406471b23fc63a2b859676954cb9c314d
Jaime Buelta <jaime.buelta@gmail.com>
2017-09-01 23:07:44 +0100

    Remove nginx and serve static files from uWSGI

--- docker/server/start_server.sh
+++ docker/server/start_server.sh
@@ -12,9 +12,7 @@ trap _term SIGTERM
 
 # Allow to define dollars in the templates
 export DOLLAR='$'
-envsubst < /opt/server/nginx.conf.template > /etc/nginx/conf.d/default.conf
 envsubst < /opt/server/uwsgi.ini.template > /opt/server/uwsgi.ini
-nginx
 uwsgi --ini /opt/server/uwsgi.ini &
 
 # We need to wait to properly catch the signal, that's why uWSGI is started
